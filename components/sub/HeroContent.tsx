7213c5c40e6ac1efe26045f5e64fffdac3af9f3f
Reda Essnoussi <71893153+redaessnoussi@users.noreply.github.com>
2024-04-05 04:41:33 +0000

    feat: stars background with animations

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -20,7 +20,7 @@ const HeroContent = () => {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="welcome-box py-[8px] px-[14px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="welcome-text text-[13px]">
